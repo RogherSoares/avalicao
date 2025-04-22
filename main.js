3309091e0d39e71f1b0af7ede9714ed916f0591d
RogherSoares <rogher_adriano@hotmail.com>
2025-04-22 20:48:38 -0300

    criando a logica para criar a lista de itens

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
-let dadosList = [];
+let itensList = [];
+let valorList = [];
+let categoriaList = [];
 
 function addDados() {
     const item = document.getElementById("itemNome").value;
     const valor = document.getElementById("itemPreco").value;
     const categoria = document.getElementById("itemCategoria").value;
 
-    if (item && valor && categoria) {
-        dadosList.push(item, valor, categoria);
+    if (item, valor, categoria) {
+        itensList.push(item);
+        valorList.push(valor);
+        categoriaList.push(categoria);
+        criarLista();
     } else {
         alert("Por favor, preencha todos os campos.");
+    }
+}
+
+function criarLista() {
+    let tabela = "<tr><th>Nome</th><th>Categoria</th><th>Valor</th></tr>"
+    for (let i = 0; i < itensList.length; i++){
+        tabela += "<tr><td>" + itensList[i] + "</td><td>" + categoriaList[i] +"</td><td>" + valorList[i] +"</td></tr>"
+        document.getElementById("tabela").innerHTML = tabela;
     }
 }
