1ffd857fc2bcf6bb7ed356040e30e435f17bcaf4
RogherSoares <rogher_adriano@hotmail.com>
2025-04-22 21:50:59 -0300

    criando o botao de editar os itens

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let itensList = [];
 let valorList = [];
 let categoriaList = [];
-let valorTotal = 0;
 
 function addDados() {
     const item = document.getElementById("itemNome").value;
@@ -21,7 +20,7 @@ function addDados() {
 function criarLista() {
     let tabela = "<tr><th>Nome</th><th>Categoria</th><th>Valor</th></tr>";
     for (let i = 0; i < itensList.length; i++) {
-        tabela += "<tr><td style='border: 1px solid rgb(0, 0, 0);'>" + itensList[i] + "</td><td style='border: 1px solid rgb(0, 0, 0);'>" + categoriaList[i] + "</td><td style='border: 1px solid rgb(0, 0, 0);'>" + valorList[i] + "</td><td><button onclick='removerItem(" + i + ")'>Remover</button></tr>";
+        tabela += "<tr><td style='border: 1px solid rgb(0, 0, 0);'>" + itensList[i] + "</td><td style='border: 1px solid rgb(0, 0, 0);'>" + categoriaList[i] + "</td><td style='border: 1px solid rgb(0, 0, 0);'>" + valorList[i] + "</td><td>"+"<button style='padding: 2px;' onclick='removerItem(" + i + ")'>Remover</button>" + "<button style='padding: 2px;' onclick='editarItem(" + i + ")'>Editar</button></td></tr>";
     }
     tabela += "<tr><td colspan='3' style='border: 1px solid rgb(0, 0, 0);'><strong>Total</strong></td><td style='border: 1px solid rgb(0, 0, 0);'><strong>" + calcularTotal() + "</strong></td></tr>";
     document.getElementById("tabela").innerHTML = tabela;
@@ -34,10 +33,21 @@ function calcularTotal() {
     }
     return valorTotal.toFixed(2);
 }
-function removerItem(index) {
-    itensList.splice(index, 1);
-    valorList.splice(index, 1);
-    categoriaList.splice(index, 1);
+function removerItem(i) {
+    itensList.splice(i, 1);
+    valorList.splice(i, 1);
+    categoriaList.splice(i, 1);
 
     criarLista();
+}
+function editarItem(i) {
+    const novoValor = prompt("Digite o novo valor para o item:", valorList[i]);
+    if (novoValor !== null && !isNaN(novoValor) && novoValor.trim() !== "") {
+
+        valorList[i] = parseFloat(novoValor).toFixed(2);
+
+        criarLista();
+    } else {
+        alert("Por favor, insira um valor válido.");
+    }
 }
